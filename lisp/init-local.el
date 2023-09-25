03e4e34830619e770588c4e38394dd7937bfe267
eriberttom_xfce_nuc <erimendz@gmail.com>
2023-09-25 06:13:08 +0300

    start adding tab-bar-mode snippets

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -103,5 +103,46 @@
       org-src-fontify-natively t
       org-src-tab-acts-natively t)
 
+;;; begin tab bar mode
+(tab-bar-mode)
+
+(defun tab-create (name)
+  "Create the NAME tab if it doesn't exist already."
+  (condition-case nil
+      (unless (equal (alist-get 'name (tab-bar--current-tab))
+                     name)
+        (tab-bar-rename-tab-by-name name name))
+    (error (tab-new)
+           (tab-bar-rename-tab name))))
+
+;; Ensure the needed tabs are created or exist; add extra to your liking
+(progn (tab-create "files")
+       (tab-create "eshells")
+       (tab-create "others"))
+
+;; show or hide tab bar???
+(setf tab-bar-show t)
+;; note if t the 3 tabs are created automatically
+
+(defun tab-bar-files ()
+  "Select the files tab-bar. Force return `t'."
+  (interactive)
+  (tab-bar-select-tab-by-name "files")
+  t)
+
+(defun tab-bar-eshells ()
+  "Select the eshells tab-bar. Force return `t'."
+  (interactive)
+  (tab-bar-select-tab-by-name "eshells")
+  t)
+
+(defun tab-bar-others ()
+  "Select the wm tab-bar. Force return `t'."
+  (interactive)
+  (tab-bar-select-tab-by-name "others")
+  t)
+
+
+
 (provide 'init-local)
 ;;; init-local.el ends here
