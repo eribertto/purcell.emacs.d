619f0380e38371b21b3295c9bc59058eb350d2ed
eriberttom_eos.eribertto-20fw0018ad <erimendz@gmail.com>
2024-02-17 14:03:40 +0300

    add lisp codes

---
+++ code-snippets/hello.lisp
@@ -0,0 +1,9 @@
+;; Happy hacking, eriberttom - Emacs ♥ you!
+
+;; 2024-02-13                              ;
+;; (print "hello")
+(defun main ()
+  (print "hello there!"))
+(defvar *cool* 123)
+(setf *cool* nil)
+(setf *cool* #'main)
