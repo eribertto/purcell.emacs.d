a1014e2264f84f3384da127d88246cf57fe6a89c
eriberttom_dellprecinixos <erimendz@gmail.com>
2024-06-22 14:36:01 +0300

    disable erring desktop variable

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -52,11 +52,8 @@
 ;; NOTE: ‘auto-save-visited-interval’ value is now 45 from 5 2024-03-23
 (menu-bar-mode t)
 (desktop-save-mode t)
-
-;;(tab-bar-mode 1)
-
+;; (desktop-load-locked-desktop t)
 ;; Show the tab-bar as soon as tab-bar functions are invoked
-;; (setopt tab-bar-show 1)
 (setq tab-bar-tab-hints t)
 (setq tab-bar-close-button-show t)
 
